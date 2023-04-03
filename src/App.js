f75d37b55decbb0979c28a565549d95edb86b161
onurgndz <gunduzonur@gmail.com>
2023-04-03 15:51:47 +0300

    storeelonmusk

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { Container } from '@chakra-ui/react';
+import './App.css';
+import Header from './components/Header';
+import ContainerProducts from "./components/Container"
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxW="120vh">
+      <Header/>
+      <ContainerProducts/>
+      <Footer/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import  {configureStore} from "@reduxjs/toolkit";
+
+import productsSlice from "./productsSlice";
+
+export const store = configureStore({
+    reducer:{
+        products:productsSlice,
+
+    },
+});
